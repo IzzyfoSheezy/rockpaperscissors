23804af3c0185508085c174f0e4304f66bfdea22
Israel Natal <israelnatal11@gmail.com>
2018-11-19 23:09:06 +0000

    day 2 rps

--- js/script.js
+++ js/script.js
@@ -9,3 +9,28 @@
 
 // DOCUMENT READY FUNCTION BELOW
 
+$("#shoot").click(function(){
+    var user= $("userChoice").val();
+    
+    $("userChoice").text(user);
+});
+
+var computerChoice;
+
+var randomNumber = Math.random();
+
+ console.log();
+ if(randomNumber > .6666666){
+     computerChoice = "Rock";
+     $("#result").html(computerChoice);
+ }
+ else if(randomNumber > .3333333){
+     computerChoice = "Paper";
+     $("#result").html(computerChoice);
+ }
+ else {
+    computerChoice = "Scissors";
+    $("#result").html(computerChoice);  
+ } 
+ 
+ 
